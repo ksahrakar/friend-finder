244d5119e119c4f20e85363d5e66f47abf7b6131
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-11-14 17:41:23 -0800

    route files updated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,42 @@
 // GET route with url "/api/friends" shows all friends in JSON format
+
 app.get("/api/friends", function(req, res) {
     res.json(friends);
-    //res.end("That was an API (GET) call");
 });
-
-app.post("/api/friends", function(req,res){
-    res.end("That was a POST");
-});
-
 
 
 
 // POST route with url "/api/friends" handles incoming survey results and compatibility logic
+
+
+
+app.post("/api/friends", function(req,res){
+    var curUser=req.body;
+    var mostCompat;
+    var dbDiff=40;
+    var dupl=false;
+    for (i=0;i<friends.length;i++){
+        if(friends[i].email==curUser.email){
+            dupl=true;break;
+        }else{
+            var curFriendDiff=0;
+            for (j=0;j<10;j++){
+                curFriendDiff+=Math.abs(friends[i].scores[j]-curUser.scores[j]);
+            }
+            if (curFriendDiff<dbDiff){
+                dbDiff=curFriendDiff;
+                mostCompat=friends[i];
+            }
+        }    
+    }
+    if (dupl){
+        mostCompat=curUser;
+        res.json(mostCompat);
+    }else{
+        curUser.inScore=dbDiff;
+        curUser.lastBestFit=dbDiff;
+        mostCompat.lastBestFit=dbDiff;
+        friends.push(curUser);
+        res.json(mostCompat);
+    }
+});
