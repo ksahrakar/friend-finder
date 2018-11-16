e9bc2aaca92da23009595aed47b7bfd7f66c8e2f
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-11-15 21:01:02 -0800

    final commit - all functional

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,45 +1,43 @@
 var express=require("express");
-var path=require("path");
 var apiRoute= express.Router();
 var friends = require("../data/friends");
 
+module.exports = function(app){
+    // GET route with url "/api/friends" shows all friends in JSON format
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
 
-// GET route with url "/api/friends" shows all friends in JSON format
-apiRoute.get("/api/friends", function(req, res) {
-    res.json(friends);
-});
-
-// POST route with url "/api/friends" handles incoming survey results and compatibility logic
-apiRoute.post("/api/friends", function(req,res){
-    var curUser=req.body;
-    var mostCompat;
-    var dbDiff=40;
-    var dupl=false;
-    for (i=0;i<friends.length;i++){
-        if(friends[i].email==curUser.email){
-            dupl=true;break;
+    // POST route with url "/api/friends" handles incoming survey results and compatibility logic
+    app.post("/api/friends", function(req,res){
+        var curUser=req.body;
+        var mostCompat;
+        var dbDiff=40;
+        var dupl=false;
+        for (i=0;i<friends.length;i++){
+            if(friends[i].email==curUser.email){
+                dupl=true;break;
+            }else{
+                var curFriendDiff=0;
+                for (j=0;j<10;j++){
+                    curFriendDiff+=Math.abs(friends[i].scores[j]-curUser.scores[j]);
+                }
+                if (curFriendDiff<dbDiff){
+                    dbDiff=curFriendDiff;
+                    mostCompat=friends[i];
+                }
+            }    
+        }
+        if (dupl){
+            mostCompat=curUser;
+            res.json(mostCompat);
         }else{
-            var curFriendDiff=0;
-            for (j=0;j<10;j++){
-                curFriendDiff+=Math.abs(friends[i].scores[j]-curUser.scores[j]);
-            }
-            if (curFriendDiff<dbDiff){
-                dbDiff=curFriendDiff;
-                mostCompat=friends[i];
-            }
-        }    
-    }
-    if (dupl){
-        mostCompat=curUser;
-        res.json(mostCompat);
-    }else{
-        curUser.inScore=dbDiff;
-        curUser.lastBestFit=dbDiff;
-        mostCompat.lastBestFit=dbDiff;
-        friends.push(curUser);
-        res.json(mostCompat);
-    }
-    console.log(friends);
-});
-
-module.exports = apiRoute;
+            curUser.inScore=dbDiff;
+            curUser.lastBestFit=dbDiff;
+            mostCompat.lastBestFit=dbDiff;
+            friends.push(curUser);
+            res.json(mostCompat);
+        }
+        console.log(friends);
+    });
+};
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var app = express();
-var path = require("path");
 var htRoute = require("./app/routing/htmlRoutes.js");
-var apiRoute = require("./app/routing/apiRoutes.js")
-
 
 var PORT = 3113;
 
@@ -11,48 +8,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/",htRoute);
 app.use("/survey",htRoute);
-app.use("/api/friends",apiRoute)
+require("./app/routing/apiRoutes.js")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("Application is listening on PORT " + PORT);
 });
-
-
-// These routes / data need to be moved to the apiRoutes file
-//-----------------------------------------------------------------
-
-// app.get("/api/friends", function(req, res) {
-//     res.json(friends);
-// });
-
-// app.post("/api/friends", function(req,res){
-//     var curUser=req.body;
-//     var mostCompat;
-//     var dbDiff=40;
-//     var dupl=false;
-//     for (i=0;i<friends.length;i++){
-//         if(friends[i].email==curUser.email){
-//             dupl=true;break;
-//         }else{
-//             var curFriendDiff=0;
-//             for (j=0;j<10;j++){
-//                 curFriendDiff+=Math.abs(friends[i].scores[j]-curUser.scores[j]);
-//             }
-//             if (curFriendDiff<dbDiff){
-//                 dbDiff=curFriendDiff;
-//                 mostCompat=friends[i];
-//             }
-//         }    
-//     }
-//     if (dupl){
-//         mostCompat=curUser;
-//         res.json(mostCompat);
-//     }else{
-//         curUser.inScore=dbDiff;
-//         curUser.lastBestFit=dbDiff;
-//         mostCompat.lastBestFit=dbDiff;
-//         friends.push(curUser);
-//         res.json(mostCompat);
-//     }
-// })
-//-----------------------------------------------------------------
